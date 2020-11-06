c7ccb7ff4eeba25b8e01e711bd4becfb78af161b
todo1017 <todo1017@outlook.com>
2020-11-06 17:18:45 +0800

    docker fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,6 @@ import { AuthGuard } from '@nestjs/passport';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @UseGuards(AuthGuard('jwt'))
   @Get()
   getHello(): string {
     return this.appService.getHello();
